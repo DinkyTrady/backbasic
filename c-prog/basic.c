5a21cc4edf74e33a9e77e54253b46c9c371c373e
DinkyTrady <DinkyTrady@kyra.com>
2025-08-02 18:20:50 +0700

    feat: Adding basic c explanation code

---
+++ c-prog/basic.c
@@ -0,0 +1,14 @@
+// include key is used for import library
+// in the below code i import stdio.h which mean
+// standard library for input output
+#include <stdio.h>
+
+// main is the key c program will execute
+// it will always need `return 0` to indicate
+// success return
+int main(int argc, char *argv[]) {
+  // C will always need semicolon `;` in the end
+  // of line (it necesserrry)
+  printf("Hello world!"); // printf are function came from library stdio.h
+  return 0;
+}
